99055a20b783e4a6c6bd746e7dd0c2f70d65bc5b
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-18 23:44:13 +0700

    for nilai variabel nilai
    for nilai variabel nilai

---
+++ Tugas10.js
@@ -0,0 +1,17 @@
+function Tugas10() {   
+    var nilai = 0;
+    
+    for (var x = 1; x <= 10; x++) {
+        console.log(nilai+=x);
+    
+  
+}
+}
+Tugas10();
+
+// var ulang = true;
+
+// for (var x= 0; x <= 10; x++) {
+//     console.log("Hello Indonesia");
+//     ulang = confirm ('ulang');
+// }
